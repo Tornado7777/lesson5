90f385283e7e96e9ce7c4231be493d978ee9432f
Ivan <tornadod@mail.ru>
2021-05-10 19:46:20 +0300

    ready quest5.1

---
+++ lesson5.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+namespace lesson5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("В введите текст для записи в файел");
+            string filename = "text.txt";
+            string textWrite = Console.ReadLine();
+            File.WriteAllText(filename, textWrite);
+            Console.WriteLine($"В файел {filename} записан текст {textWrite}");
+        }
+    }
+}
